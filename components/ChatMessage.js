14ffa6d085ae3bdf35c20ddc075d64b3e2a435ed
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-08-02 22:08:26 +0500

    changed the minimized status

--- components/ChatMessage.js
+++ components/ChatMessage.js
@@ -1,9 +1,10 @@
 import { FacebookProvider, CustomChat } from "react-facebook";
 
 export default function ChatMessage() {
+  console.log("called");
   return (
     <FacebookProvider appId="1035059091188504" chatSupport>
-      <CustomChat pageId="105822544169742" minimized={false} />
+      <CustomChat pageId="105822544169742" minimized={true} />
     </FacebookProvider>
     // <!-- Messenger Chat Plugin Code -->
     // <div id="fb-root"></div>
