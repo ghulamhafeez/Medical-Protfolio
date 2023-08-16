873e0303a7d1808424d9c6a174a2918a18edf5c3
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-08-16 23:42:00 +0500

    added default export in blog detail

--- pages/blog/blog-detail.js
+++ pages/blog/blog-detail.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Grid } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
-export const BlogDetail = () => {
+export default function BlogDetail() {
   return (
     <Grid>
       <Grid
@@ -22,4 +22,4 @@ export const BlogDetail = () => {
       </Grid>
     </Grid>
   );
-};
+}
